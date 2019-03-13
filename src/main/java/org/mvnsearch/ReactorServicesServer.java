d8444ac773a95c43d411dcb7c800bc34957ae56d
linux_china <libing.chen@gmail.com>
2019-03-13 10:51:33 -0700

    polishing

--- src/main/java/org/mvnsearch/ReactorServicesServer.java
+++ src/main/java/org/mvnsearch/ReactorServicesServer.java
@@ -2,6 +2,7 @@ package org.mvnsearch;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import org.mvnsearch.application.ReactiveAccountServiceGrpcImpl;
 
 /**
  * Reactor services server
@@ -9,7 +10,7 @@ import io.grpc.ServerBuilder;
  * @author linux_china
  */
 public class ReactorServicesServer {
-    
+
     public static void main(String[] args) throws Exception {
         // Start the server
         Server server = ServerBuilder.forPort(50051).addService(new ReactiveAccountServiceGrpcImpl()).build().start();
