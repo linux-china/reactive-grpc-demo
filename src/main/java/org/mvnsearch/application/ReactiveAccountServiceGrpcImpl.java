f7fb42ba19e9f14e404591dc8a4e69e9e6d1a4f6
linux_china <libing.chen@gmail.com>
2019-03-13 10:51:23 -0700

    change logic to reactive service handler

--- src/main/java/org/mvnsearch/application/ReactiveAccountServiceGrpcImpl.java
+++ src/main/java/org/mvnsearch/application/ReactiveAccountServiceGrpcImpl.java
@@ -1,5 +1,7 @@
-package org.mvnsearch;
+package org.mvnsearch.application;
 
+import org.mvnsearch.domain.AccountService;
+import org.mvnsearch.domain.impl.AccountServiceImpl;
 import org.mvnsearch.service.AccountResponse;
 import org.mvnsearch.service.GetAccountRequest;
 import org.mvnsearch.service.ReactorAccountServiceGrpc;
@@ -11,9 +13,13 @@ import reactor.core.publisher.Mono;
  * @author linux_china
  */
 public class ReactiveAccountServiceGrpcImpl extends ReactorAccountServiceGrpc.AccountServiceImplBase {
+    //reactive service handler
+    private AccountService accountService = new AccountServiceImpl();
 
     @Override
     public Mono<AccountResponse> findAccountById(Mono<GetAccountRequest> request) {
-        return request.map(getAccountRequest -> AccountResponse.newBuilder().setId(getAccountRequest.getId()).setNick("linux_china").build());
+        return request.map(GetAccountRequest::getId)
+                .flatMap(accountService::findById)
+                .map(AccountMapper.INSTANCE::pojoToProtobuf);
     }
 }
